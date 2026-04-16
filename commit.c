40624b5776d642850e3b326a76f7f008eb723e7d
Shreya <shreyahegde911@gmail.com>
2026-04-16 22:34:43 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -194,8 +194,38 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0)
+        return -1;
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
+        Commit commit;
+    commit.tree = tree_id;
+
+    if (has_parent) {
+        commit.has_parent = 1;
+        commit.parent = parent_id;
+    } else {
+        commit.has_parent = 0;
+    }
+
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+    commit.timestamp = time(NULL);
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
+        void *data = NULL;
+    size_t len = 0;
+
+    if (commit_serialize(&commit, &data, &len) != 0)
+        return -1;
+
+    if (object_write(OBJ_COMMIT, data, len, commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+        if (head_update(commit_id_out) != 0)
+        return -1;
+
+    return 0;
 }
